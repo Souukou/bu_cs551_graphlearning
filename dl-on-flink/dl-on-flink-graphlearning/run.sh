47061c80da53505911256e7fce69bf02ecd6e62e
Adrish Dey <rickdey1998@gmail.com>
2023-03-19 18:36:44 -0400

    fixing minor typo

--- dl-on-flink/dl-on-flink-graphlearning/run.sh
+++ dl-on-flink/dl-on-flink-graphlearning/run.sh
@@ -4,4 +4,4 @@ pushd dl-on-flink/
 mvn -DskipTests -Drat.skip=true -Dcheckstyle.skip install
 popd
 start-cluster.sh
-flink run -pyfs graphlearning/ dl-on-flink/dl-on-flink-graphlearning/target/GraphOperator.jar --pyscript graphlearning/main.py
+flink run -pyfs graphlearning/ dl-on-flink/dl-on-flink-graphlearning/target/GraphOperators.jar --pyscript graphlearning/main.py
