e91cc813d11c0e2b9abeda092a91e8619a85d0c5
Adrish Dey <rickdey1998@gmail.com>
2023-03-19 18:21:35 -0400

    fixing runner script

--- dl-on-flink/dl-on-flink-graphlearning/run.sh
+++ dl-on-flink/dl-on-flink-graphlearning/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd dl-on-flink/dl-on-flink-graphlearning
-mvn -DskipTests -Drat.skip=true install
+pushd dl-on-flink/
+mvn -DskipTests -Drat.skip=true -Dchecktyle.skip install
+popd
 start-cluster.sh
 flink run -pyfs graphlearning/ dl-on-flink/dl-on-flink-graphlearning/target/GraphOperator.jar --pyscript graphlearning/main.py
