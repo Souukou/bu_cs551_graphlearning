2d207d3fa4e4064b6f8b30770086ef4348b97c1f
iasonas <nikolaou@bu.edu>
2023-03-19 17:32:37 -0400

    Add InputGraphChange class

--- flink-main/src/main/java/org/apache/flink/quickstart/InputGraphChange.java
+++ flink-main/src/main/java/org/apache/flink/quickstart/InputGraphChange.java
@@ -1,2 +1,11 @@
-package org.apache.flink.quickstart;public class InputGraphChange {
+package org.apache.flink.quickstart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+@Getter
+@AllArgsConstructor
+public class InputGraphChange {
+    private Tuple2<String, Integer> inputGraphChange;
 }
