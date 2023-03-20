924e833d3862d54a56b2cd767f45a98c6ff32bea
iasonas <nikolaou@bu.edu>
2023-03-20 16:43:26 -0400

    MapToRow: Dummy Conversion

--- dl-on-flink/dl-on-flink-graphlearning/src/main/java/org/flinkextended/team2/graph/MapToRow.java
+++ dl-on-flink/dl-on-flink-graphlearning/src/main/java/org/flinkextended/team2/graph/MapToRow.java
@@ -4,10 +4,10 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.types.Row;
 
-public class MapToRow implements MapFunction<GraphChange, Row> {
+public class MapToRow implements MapFunction<Tuple5<Integer, Short, Integer, byte[], String>, Row> {
     @Override
-    public Row map(GraphChange graphChange) {
-        Tuple5<Integer, Short, Integer, byte[], String> tuple = graphChange.getInputGraphChange();
+    public Tuple5<Integer, Short, Integer, byte[], String> map(Tuple5<Integer, Short, Integer, byte[], String> tuple) {
+//        Tuple5<Integer, Short, Integer, byte[], String> tuple = graphChange.getInputGraphChange();
         return Row.of(tuple.f0, tuple.f4, tuple.f2, tuple.f3);
     }
 
