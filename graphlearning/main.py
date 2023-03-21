071c4349a7bcaee7ca0c64d576fa1aae843d12a0
Adrish Dey <rickdey1998@gmail.com>
2023-03-20 20:33:04 -0400

    updating experimental main.py

--- graphlearning/main.py
+++ graphlearning/main.py
@@ -1,12 +1,15 @@
 import utils 
 import model
 from new_context import NewPyTorchContext
+import torch
 
 
 def train(context):
   print("Inside train")
   pc = NewPyTorchContext(context)
-  pc.get_dataset_from_flink()
+  dataloader = torch.utils.data.DataLoader(pc.get_dataset_from_flink())
+  for data in dataloader:
+    break
 
 def inference(context):
   print("inside inference")
