922f98e09e81aa30f67f560fed7941dc20098d46
Adrish Dey <rickdey1998@gmail.com>
2023-03-21 16:33:52 -0400

    connecting RocksDB stream with training

---
+++ dl-on-flink/dl-on-flink-graphlearning/src/main/java/org/flinkextended/team2/graph/NodeReader.java
@@ -0,0 +1,32 @@
+package org.flinkextended.team2.graph;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.rocksdb.*;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * findFeatures(Integer nodeID, RocksDB db) input: query node's id; a RocksDB instance output: flink
+ * Tuple3(Integer: mask, Integer: label, byte[]: embedding)
+ */
+public interface NodeReader {
+    public static Tuple3 findFeatures(Integer nodeID, RocksDB db) throws RocksDBException {
+        byte[] keyByte = Integer.toString(nodeID).getBytes();
+        byte[] value = db.get(keyByte);
+
+        if (value == null) {
+            System.out.println("Value not found for key: " + new String(keyByte));
+        }
+        int keyInt = nodeID;
+        int mask = ByteBuffer.wrap(Arrays.copyOfRange(value, 0, 7)).getInt();
+        int label = ByteBuffer.wrap(Arrays.copyOfRange(value, 8, 15)).getInt();
+        byte[] embedding = Arrays.copyOfRange(value, 16, value.length);
+        List<Byte> embeddingList = Arrays.asList(ArrayUtils.toObject(embedding));
+        Tuple3<Integer, Integer, List<Byte>> entry = new Tuple3<>(mask, label, embeddingList);
+        return entry;
+    }
+}
